c2d4ec82a4e78fe1d07814c44b0afc36a09636e3
Léo Belini <leobelini96@gmail.com>
2023-12-02 16:31:43 -0300

    feat: update handle structure to separate package

--- router/routes.go
+++ router/routes.go
@@ -1,43 +1,17 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/leobelini-studies/arthur-404-dev_api_moderna_golang/handler"
 )
 
 func initializeRoutes(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
 	{
-
-		v1.GET("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "GET Opening",
-			})
-		})
-
-		v1.POST("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "POST Opening",
-			})
-		})
-
-		v1.DELETE("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "DELETE Opening",
-			})
-		})
-
-		v1.PUT("/opening", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "PUT Opening",
-			})
-		})
-
-		v1.GET("/openings", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "GET Openings",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
