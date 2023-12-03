1bb9579746295ecf78c114024a5d2b56344165a6
Léo Belini <leobelini96@gmail.com>
2023-12-02 21:25:08 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"github.com/leobelini-studies/arthur-404-dev_api_moderna_golang/config"
 	"github.com/leobelini-studies/arthur-404-dev_api_moderna_golang/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+
+	// Initialize Logger
+	logger = config.NewLogger("main")
+
+	// Initialize Config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config.Init() error: %s", err)
+		panic(err)
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
